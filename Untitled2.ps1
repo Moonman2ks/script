a44241b744ed7de24292d5474ca98ea11e252632
Moonman2ks <55389345+Moonman2ks@users.noreply.github.com>
2019-10-14 16:26:13 +0300

    Add files via upload

---
+++ Untitled2.ps1
@@ -0,0 +1,7 @@
+﻿$Depth = 1
+$Path = 'C:\Users'
+
+$Levels = '/*' * $Depth
+$xml = Get-ChildItem -Directory $Path/$Levels
+$xml | Export-Clixml C:\Users\gytis.ka8869\Desktop\prisijungevisi.xml
+Read-Host -Prompt "Press Enter to exit"
