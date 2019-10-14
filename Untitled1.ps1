54da6e3a003b290da359bebf80f1816d90a52197
Moonman2ks <55389345+Moonman2ks@users.noreply.github.com>
2019-10-14 16:14:36 +0300

    Add files via upload

---
+++ Untitled1.ps1
@@ -0,0 +1,13 @@
+﻿#komentaras
+<#
+
+#>
+#Get-WmiObject -Class Win32_UserAccount
+#$xml = gwmi Win32_LoggedOnUser | Select Antecedent
+#Returns a WindowsIdentity object that represents the current Windows user.
+$xml =[System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+#Trims the output (removes K and / symbols)
+$xml = $xml.Trim("K","\")
+#Creates an XML-based representation of an object and stores it into a file
+$xml | Export-Clixml C:\Users\gytis.ka8869\Desktop\prisijunge.xml
+Read-Host -Prompt "Press Enter to exit"
